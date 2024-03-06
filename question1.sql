f7fec0397e4e0725c82f78f88aa11b853a00dc44
1-0-GO <martinsfrancisco01@gmail.com>
2024-03-06 23:06:06 +0000

    Failed attempt to load the data into the table.  Correct query to get the number of rows in each partition

--- question1.sql
+++ question1.sql
@@ -1,38 +1,42 @@
 -- get insight into table
 SELECT 
 	top(100) * 
-FROM 
-	Energy.MonthlyConsumption;
+FROM Energy.MonthlyConsumption;
 
 -- check all different years in Energy.MonthlyConsumption
 SELECT DISTINCT
-	year
-FROM
-	Energy.MonthlyConsumption;
+	Year
+FROM Energy.MonthlyConsumption;
 
 -- create the partition function (year <= 2020, year > 2020 AND year <= 2021, year > 2021 AND year <= 2022, year > 2022)
 CREATE PARTITION FUNCTION
     MonthlyConsumption_PartitionByYear
 	(INT)
-AS RANGE LEFT FOR VALUES 
-	(2020, 2021, 2022);
+AS RANGE LEFT FOR VALUES (2020, 2021, 2022);
 
 -- create the partitions scheme on the primary filegroup
 CREATE PARTITION SCHEME 
 	PartScheme_MonthlyConsumption
 AS PARTITION
 	MonthlyConsumption_PartitionByYear 
-ALL TO
+ALL TO 
 	( [PRIMARY] );
+
+CREATE UNIQUE CLUSTERED INDEX PK_MonthlyConsumption ON Energy.MonthlyConsumption 
+(Year, Month, VoltageLevel, DistrictMunicipalityParishCode)
+WITH (DROP_EXISTING = ON) 
+ON PartScheme_MonthlyConsumption(Year)
 
 -- Number of records per partition
 SELECT 
-	$PARTITION.MonthlyConsumption_PartitionByYear(Year) AS PartitionNumber,
-    COUNT(*) AS RecordCount
-FROM 
-	Energy.MonthlyConsumption
-GROUP BY 
-	$PARTITION.MonthlyConsumption_PartitionByYear(Year);
+	p.partition_number AS partition_number,
+	f.name AS file_group, 
+	p.rows AS row_count
+FROM sys.partitions p
+JOIN sys.destination_data_spaces dds ON p.partition_number = dds.destination_id
+JOIN sys.filegroups f ON dds.data_space_id = f.data_space_id
+WHERE OBJECT_NAME(OBJECT_ID) = 'MonthlyConsumption'
+ORDER BY partition_number;
 
 
 
